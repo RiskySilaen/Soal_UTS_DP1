235c96ecf78b06a78c667f9525138040a7b287b0
Risky Silaen <riskyjonalson@students.usu.ac.id>
2024-11-02 11:47:20 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,23 @@
-//ketik kode jawaban noo 3 kamu disini
+program bintang; // nama program
+uses crt;
+var n,a,b: integer; //mendeklarasikan variabel
+begin
+    clrscr; // membersihkan layar
+    write('masukkan nilai n = '); readln(n); // masukkan nilai n
+    if n mod 2=0 then 
+    writeln('nilai n harus bilangan ganjil!!') //memberikan pesan kesalahan
+    else 
+    begin 
+    for a:=0 to n-1 do 
+    begin 
+        for b:=0 to n-1 do 
+    begin
+        if (a=b) or (a=n-b-1) then 
+        write('*') //menampilkan bintang
+        else 
+        write(' '); 
+    end;
+    writeln; 
+    end;
+    end;
+end.
