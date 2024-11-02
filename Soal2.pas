a9c0424dc0b088222575a00b75bd7e5714a6bccf
Risky Silaen <riskyjonalson@students.usu.ac.id>
2024-11-03 00:25:48 +0700

    Update Soal2.pas

--- Soal2.pas
+++ Soal2.pas
@@ -1,1 +1,44 @@
-//ketik kode jawaban no 2 kamu disini
+program pakstephen;
+uses crt;
+
+var
+  jumlahPembeli, jumlahSayur, hargaSayur, jumlahBeli, i, j: Integer;
+  totalBelanja: Real; // mendeklarasikan variabel
+
+begin
+  clrscr;
+  
+  // masukkan jumlah pembeli
+  Write('Masukkan jumlah pembeli: ');
+  ReadLn(jumlahPembeli);
+
+  
+  for i := 1 to jumlahPembeli do
+  begin
+    WriteLn('Pembeli ke-', i, ':');
+    
+    // masukkan jumlah jenis sayur yang dibeli
+    Write('Masukkan jumlah jenis sayur yang dibeli: ');
+    ReadLn(jumlahSayur);
+    
+    totalBelanja := 0; 
+    
+    
+    for j := 1 to jumlahSayur do
+    begin
+      // masukkan harga dan jumlah sayur yang dibeli
+      Write('Masukkan harga sayur ke-', j, ': ');
+      ReadLn(hargaSayur);
+      Write('Masukkan jumlah sayur ke-', j, ': ');
+      ReadLn(jumlahBeli);
+      
+      // Menghitung total untuk jenis sayur
+      totalBelanja := totalBelanja + (hargaSayur * jumlahBeli);
+    end;
+    
+    // Output total belanja untuk pembeli
+    WriteLn('Total belanja pembeli ke-', i, ': Rp ', totalBelanja:0:2);
+  end;
+
+  ReadLn;
+end.
